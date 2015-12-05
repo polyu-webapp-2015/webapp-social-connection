88dfdb97297fca12535f7433950423be24f304e7
Beeno Tung <aabbcc1241@yahoo.com.hk>
2015-12-05 21:30:47 +0800

    removed deprecated php scripts (under public and webapp-server); working on api checking

--- overall/script/ResultCodeEnum.sh
+++ overall/script/ResultCodeEnum.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo '<?php'
-echo 'class ResultCode'
+echo 'class ResultCodeEnum'
 echo '{'
 echo '  const _ = "resultCode";'
 cat ../webapp-server/app/models/idl/social_connection/ResultCodeEnum.java  | grep 'final' | grep -v ResultCode | sed 's/public static final int/const/g'
