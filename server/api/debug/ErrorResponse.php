a03d1820ca67023f589e7f3e7d08bb9f7c3971f9
Beeno Tung <aabbcc1241@yahoo.com.hk>
2015-12-06 18:38:29 +0800

    restored ErrorResponse.php

---
+++ server/api/debug/ErrorResponse.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: beenotung
+ * Date: 12/6/15
+ * Time: 6:31 PM
+ */
+class ErrorResponse
+{
+
+    public static function response($result_code, $msg)
+    {
+        header('HTTP/1.0 400 Bad Request', true, 400);
+        $output = [
+            APIActor::__result_code => $result_code,
+            APIActor::__reason => $msg
+        ];
+        die(json_encode($output));
+    }
+}
