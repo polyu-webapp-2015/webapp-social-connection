cc92c338a72d59d314f4fd3c7cf0194c3091fcac
Beeno Tung <aabbcc1241@yahoo.com.hk>
2015-12-08 22:21:36 +0800

    updated javascript enum style

--- overall/script/make_enum_number_function_js.sh
+++ overall/script/make_enum_number_function_js.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 echo 'var '$1'Enum = function () {'
 echo '    this._ = "'$1'";'
-cat ../webapp-server/app/models/idl/social_connection/$1Enum.java  | grep 'final' | grep -v idl | sed 's/public static final int /  this./g'
+cat ../webapp-server/app/models/idl/social_connection/$1Enum.java  | grep 'final' | grep -v idl | sed 's/public static final int _/  this./g'
 #cat ../webapp-server/app/models/idl/social_connection/$1Enum.java | grep 'final' | grep 'idl' | awk '{print "    this._"$5" = \""$5"\";" }'
 echo '};'
+echo 'var '$1' = new '$1'Enum();'
