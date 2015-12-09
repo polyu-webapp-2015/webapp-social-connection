47c5a3c31cd841253b7613ec57249fd1d09345c3
Beeno Tung <aabbcc1241@yahoo.com.hk>
2015-12-10 00:19:41 +0800

    finished create exhibition actor fixed event table PK AI

--- server/api/actor/CreateExhibitionActor.php
+++ server/api/actor/CreateExhibitionActor.php
@@ -4,21 +4,20 @@
  * Created by IntelliJ IDEA.
  * User: beenotung
  */
-class CreateSessionActor extends Actor
+class CreateExhibitionActor extends Actor
 {
-    public $name = "CreateSession";
+    public $name = "CreateExhibition";
     public $params = array(
         Event_Fields::__venue_id => 123,
         Event_Fields::__event_time => '2015-12-09 20:25:21',
         Event_Fields::__subject => "Eco-friendly Hotel in Asia",
-        Event_Fields::__description => "This session discuss about the eco-friendly hotels in Asia. ...",
-        Session_Fields::__quota => 200
+        Event_Fields::__description => "This Exhibition display about the eco-friendly hotels in Asia. ...",
     );
     public $output = [
         APIFieldEnum::_ResultCode => ResultCodeEnum::_Success,
         Event_Fields::__event_id => 123
     ];
-    public $desc = "create conference session, this is only allowed to admin and helper";
+    public $desc = "create conference Exhibition, this is only allowed to admin and helper";
 
     public function handle($data)
     {
@@ -34,22 +33,21 @@ class CreateSessionActor extends Actor
             Event_Fields::__subject,
             Event_Fields::__description
         ]);
-        $field_array[Event_Fields::__event_type] = event_type_Enum::__S;
+        $field_array[Event_Fields::__event_type] = event_type_Enum::__E;
         $field_array[Event_Fields::__creator_account_id] = $account_id;
         $field_array[Event_Fields::__editor_account_id] = $account_id;
         $result = DatabaseHelper::table_insert(Event_Fields::_, $field_array);
         $event_id = DatabaseHelper::$_pdo->lastInsertId();
         log_object_from_named($event_id, "new Event id");
-        /* create Conference Session */
+        /* create Conference Exhibition */
         $field_array = [];
-        $field_array[Session_Fields::__event_id] = $event_id;
-        $field_array[Session_Fields::__quota] = $this->params[Session_Fields::__quota];
-        DatabaseHelper::table_insert(Session_Fields::_, $field_array);
+        $field_array[Exhibition_Fields::__event_id] = $event_id;
+        DatabaseHelper::table_insert(Exhibition_Fields::_, $field_array);
 //        $event_id = DatabaseHelper::$_pdo->lastInsertId();
-        log_object_from_named($event_id, "new Session id");
+        log_object_from_named($event_id, "new Exhibition id");
         $this->output[Event_Fields::__event_id] = $event_id;
         return $this->output;
     }
 }
 
-addAPI(new CreateSessionActor());
+addAPI(new CreateExhibitionActor());
