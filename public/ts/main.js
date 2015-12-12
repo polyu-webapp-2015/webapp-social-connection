d92549bc97a8a031db06f4b3c17ba90a698547a5
Beeno Tung <aabbcc1241@yahoo.com.hk>
2015-12-13 01:46:25 +0800

    removed temp built models

---
+++ public/ts/main.js
@@ -0,0 +1,234 @@
+///<reference path="../js/enum/APIFieldEnum.ts"/>
+///<reference path="../js/enum/ResultCodeEnum.ts"/>
+///<reference path="../js/api_list.ts"/>
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+var social_connection;
+(function (social_connection) {
+    /** @unsafe should not disclose this sensitive info */
+    var config;
+    (function (config) {
+        var session_id;
+        config.login = false;
+        function save_login(session_id) {
+            this.session_id = session_id;
+            config.login = true;
+        }
+        config.save_login = save_login;
+        function getSessionId() {
+            if (config.login)
+                return this.session_id;
+            else
+                throw new debug.IllegalStatusError();
+        }
+        config.getSessionId = getSessionId;
+    })(config = social_connection.config || (social_connection.config = {}));
+    var ui;
+    (function (ui) {
+        //import DiscussBoard = social_connection.model.DiscussBoard;
+        ui.onLogin = function (resultCode, data) {
+            if (resultCode = ResultCode.Success) {
+                comm.log("login success");
+                var sessionId = data[APIField.session_id];
+                comm.log("the session id is " + sessionId);
+                config.save_login(sessionId);
+            }
+            else {
+                comm.log("failed to login");
+            }
+        };
+    })(ui || (ui = {}));
+    var asynchronous_logic;
+    (function (asynchronous_logic) {
+        //TODO test import DiscussBoard_stub = stub.DiscussBoard_stub;
+        function login(id, password) {
+            comm.indent(1);
+            comm.log("try to login");
+            var data = {};
+            data[APIField.emailOrPhoneNum] = id;
+            data[APIField.password] = password;
+            api.api_call(_api_Login, data, ui.onLogin);
+            comm.log("sent login command");
+            comm.indent(-1);
+        }
+        asynchronous_logic.login = login;
+    })(asynchronous_logic = social_connection.asynchronous_logic || (social_connection.asynchronous_logic = {}));
+})(social_connection || (social_connection = {}));
+var debug;
+(function (debug) {
+    var IllegalStatusError = (function (_super) {
+        __extends(IllegalStatusError, _super);
+        function IllegalStatusError() {
+            _super.apply(this, arguments);
+            this.name = "IllegalStatusError";
+        }
+        return IllegalStatusError;
+    })(Error);
+    debug.IllegalStatusError = IllegalStatusError;
+    var APIFailedError = (function (_super) {
+        __extends(APIFailedError, _super);
+        function APIFailedError(result) {
+            _super.call(this);
+            this.name = "APICallFailed";
+            comm.log(result);
+        }
+        return APIFailedError;
+    })(Error);
+    debug.APIFailedError = APIFailedError;
+})(debug || (debug = {}));
+var comm;
+(function (comm) {
+    var indentText = " ";
+    var indentCount = 0;
+    function getIndentPrefix() {
+        var indent = "";
+        for (var i = 0; i < indentCount; i++)
+            indent += indentText;
+        return indent;
+    }
+    function log(message) {
+        if (message === void 0) { message = ""; }
+        message = message.toString();
+        message = getIndentPrefix() + message;
+        console.log(message);
+        var element = document.createElement("span");
+        element.innerHTML = "<pre>\n" + message + "</pre>";
+        document.body.appendChild(element);
+    }
+    comm.log = log;
+    function indent(delta) {
+        indentCount += delta;
+    }
+    comm.indent = indent;
+})(comm || (comm = {}));
+var api;
+(function (api) {
+    var _api_url = "http://localhost:8000/api/main.php";
+    function api_call(api_action, data, success, failMessage) {
+        if (failMessage === void 0) { failMessage = "Failed to call api " + api_action; }
+        comm.log("calling api " + api_action);
+        if (social_connection.config.login) {
+            data[APIField.session_id] = social_connection.config.getSessionId();
+        }
+        var payload = {
+            "action": api_action,
+            "data": data
+        };
+        function failed(data, status, headers, config) {
+            comm.log(failMessage);
+            comm.log(data);
+            //alert('internal error');
+            alert(failMessage);
+            alert(data);
+        }
+        if (api.$http != null) {
+            api.$http.post(_api_url, {
+                action: api_action,
+                data: JSON.stringify(data)
+            })
+                .success(function (data, status, headers, config) {
+                var resultCode = data[APIField.result_code];
+                success(resultCode, data);
+                //if (resultCode == ResultCode.Success) {
+                // success(resultCode, data);
+                //}
+                //else
+                // failed(data, status, headers, config);
+            })
+                .error(function (data, status, headers, config) {
+                failed(data, status, headers, config);
+            });
+        }
+        else {
+            $.ajax({
+                type: "POST",
+                url: _api_url,
+                crossDomain: true,
+                dataType: 'jsonp',
+                success: function (e) {
+                    try {
+                        var result = JSON.parse(e);
+                        try {
+                            var resultCode = result[APIField.result_code];
+                            var data = result[APIField.data];
+                            success(resultCode, data);
+                        }
+                        catch (exception) {
+                            comm.log("failed to process api result");
+                            comm.log(exception);
+                            comm.log(e);
+                        }
+                    }
+                    catch (exception) {
+                        comm.log("failed to parse json from api result");
+                        comm.log(exception);
+                        comm.log(e);
+                    }
+                }
+            }).error(function (result) {
+                comm.log(failMessage);
+                comm.log(result);
+            });
+        }
+    }
+    api.api_call = api_call;
+})(api || (api = {}));
+var task;
+(function (task_1) {
+    var TaskQueue = (function () {
+        function TaskQueue(taskQueue) {
+            if (taskQueue === void 0) { taskQueue = []; }
+            this.taskQueue = taskQueue;
+        }
+        TaskQueue.prototype.startTasks = function () {
+            while (this.hasNextTask()) {
+                var task = this.nextTask();
+                task();
+            }
+        };
+        TaskQueue.prototype.addTask = function (task) {
+            this.taskQueue.push(task);
+        };
+        TaskQueue.prototype.hasNextTask = function () {
+            return this.taskQueue.length > 0;
+        };
+        TaskQueue.prototype.nextTask = function () {
+            if (this.hasNextTask()) {
+                return this.taskQueue.shift();
+            }
+            else {
+                throw new NoNextTaskError();
+            }
+        };
+        return TaskQueue;
+    })();
+    task_1.TaskQueue = TaskQueue;
+    var TaskError = (function (_super) {
+        __extends(TaskError, _super);
+        function TaskError(message) {
+            _super.call(this, message);
+            this.message = message;
+            this.name = "TaskError";
+        }
+        return TaskError;
+    })(Error);
+    var NoNextTaskError = (function (_super) {
+        __extends(NoNextTaskError, _super);
+        function NoNextTaskError() {
+            _super.apply(this, arguments);
+            this.name = "NoNextTaskError";
+        }
+        return NoNextTaskError;
+    })(TaskError);
+})(task || (task = {}));
+function main_init() {
+    comm.log("stub_test:start");
+    var id = "98765432";
+    var password = "123456";
+    social_connection.asynchronous_logic.login(id, password);
+    comm.log("stub_test:end");
+}
+//# sourceMappingURL=main.js.map
---
+++ public/ts/main.ts
@@ -0,0 +1,264 @@
+///<reference path="../js/enum/APIFieldEnum.ts"/>
+///<reference path="../js/enum/ResultCodeEnum.ts"/>
+///<reference path="../js/api_list.ts"/>
+
+module social_connection {
+  /** @unsafe should not disclose this sensitive info */
+  export module config {
+    var session_id:string;
+    export var login:boolean = false;
+
+
+    export function save_login(session_id:string) {
+      this.session_id = session_id;
+      login = true;
+    }
+
+    export function getSessionId():string {
+      if (login)
+        return this.session_id;
+      else
+        throw new debug.IllegalStatusError();
+    }
+  }
+  module ui {
+    //import DiscussBoard = social_connection.model.DiscussBoard;
+    export var onLogin:api.APICallback<any> = function (resultCode:string, data:any):any {
+      if (resultCode = ResultCode.Success) {
+        comm.log("login success");
+        var sessionId = data[APIField.session_id];
+        comm.log("the session id is " + sessionId);
+        config.save_login(sessionId);
+        //asynchronous_logic.getAllDiscussBoard();
+      } else {
+        comm.log("failed to login");
+      }
+    };
+  }
+  export module model {
+    //export class Profile {
+    //  public first_name:string;
+    //  public last_name:string;
+    //
+    //  public toString():string {
+    //    return this.first_name + " " + this.last_name;
+    //  }
+    //
+    //  constructor(firstName:string, lastName:string) {
+    //    this.first_name = firstName;
+    //    this.last_name = lastName;
+    //  }
+    //
+    //  static parse_list:api.APICallback<Profile[]> = function (resultCode:string, data:any):model.Profile[] {
+    //    if (resultCode != ResultCode.Success)
+    //      throw debug.IllegalStatusError;
+    //    var profile_raws:any[] = data[APIField.element_array];
+    //    var profile_list = profile_raws.map(function (raw) {
+          //return new Profile(raw["first_name"], raw["last_name"]);
+        //});
+        //return profile_list;
+      //}
+    //}
+    //export class DiscussBoard {
+    //  public desc:string;
+    //
+    //  constructor(desc:string) {
+    //    this.desc = desc;
+    //  }
+    //
+    //  static parse_list:api.APICallback<DiscussBoard[]> = function (resultCode:string, data:any):DiscussBoard[] {
+    //    comm.log("parsing discuss board list");
+        //var list:DiscussBoard[] = [];
+        //list.push(new DiscussBoard("123"));
+        //return list;
+      //}
+    //}
+  }
+  export module asynchronous_logic {
+    //TODO test import DiscussBoard_stub = stub.DiscussBoard_stub;
+    export function login(id:string, password:string) {
+      comm.indent(1);
+      comm.log("try to login");
+      var data = {};
+      data[APIField.emailOrPhoneNum] = id;
+      data[APIField.password] = password;
+      api.api_call(_api_Login, data, ui.onLogin);
+      comm.log("sent login command");
+      comm.indent(-1);
+    }
+
+    //export function getAllDiscussBoard() {
+    //  comm.log("try to get all discuss board");
+    //  var data = {};
+    //  data[APIField.id_array] = [];
+    //  api.api_call(_api_GetProfileList, data, model.Profile.parse_list);
+    //}
+  }
+}
+module debug {
+  export class IllegalStatusError extends Error {
+    public name = "IllegalStatusError";
+  }
+  export class APIFailedError extends Error {
+    public name = "APICallFailed";
+
+    constructor(result:any) {
+      super();
+      comm.log(result);
+    }
+  }
+}
+module comm {
+  var indentText = " ";
+  var indentCount = 0;
+
+  function getIndentPrefix():string {
+    var indent = "";
+    for (let i = 0; i < indentCount; i++)
+      indent += indentText;
+    return indent;
+  }
+
+  export function log(message:any = "") {
+    message = message.toString();
+    message = getIndentPrefix() + message;
+    console.log(message);
+    var element = document.createElement("span");
+    element.innerHTML = "<pre>\n" + message + "</pre>";
+    document.body.appendChild(element);
+  }
+
+  export function indent(delta:number) {
+    indentCount += delta;
+  }
+}
+module api {
+  export var $http:any;
+  declare var $:any;
+  export interface APICallback<T> {
+    (resultCode:string, data:any):T;
+  }
+
+  var _api_url = "http://localhost:8000/api/main.php";
+
+  export function api_call<T>(api_action:string, data:any, success:APICallback<T>, failMessage = "Failed to call api " + api_action) {
+    comm.log("calling api " + api_action);
+    if (social_connection.config.login) {
+      data[APIField.session_id] = social_connection.config.getSessionId();
+    }
+    var payload = {
+      "action": api_action,
+      "data": data
+    };
+
+    function failed(data:any, status:any, headers:any, config:any) {
+      comm.log(failMessage);
+      comm.log(data);
+      //alert('internal error');
+      alert(failMessage);
+      alert(data);
+    }
+
+    if ($http != null) {
+      $http.post(_api_url, {
+          action: api_action,
+          data: JSON.stringify(data)
+        })
+        .success(function (data, status, headers, config) {
+          var resultCode = data[APIField.result_code];
+          success(resultCode, data);
+          //if (resultCode == ResultCode.Success) {
+          // success(resultCode, data);
+          //}
+          //else
+          // failed(data, status, headers, config);
+        })
+        .error(function (data, status, headers, config) {
+          failed(data, status, headers, config);
+        });
+    } else {
+      $.ajax({
+        type: "POST",
+        url: _api_url,
+        crossDomain: true,
+        dataType: 'jsonp',
+        success: function (e) {
+          try {
+            var result = JSON.parse(e);
+            try {
+              var resultCode = result[APIField.result_code];
+              var data = result[APIField.data];
+              success(resultCode, data);
+            } catch (exception) {
+              comm.log("failed to process api result");
+              comm.log(exception);
+              comm.log(e);
+            }
+          } catch (exception) {
+            comm.log("failed to parse json from api result");
+            comm.log(exception);
+            comm.log(e);
+          }
+        }
+      }).error(function (result:any) {
+        comm.log(failMessage);
+        comm.log(result);
+      });
+    }
+  }
+}
+module task {
+  export interface Task {
+    ():any;
+  }
+  export class TaskQueue {
+    constructor(taskQueue:Task[] = []) {
+      this.taskQueue = taskQueue;
+    }
+
+    private taskQueue:Task[];
+
+    public startTasks() {
+      while (this.hasNextTask()) {
+        var task = this.nextTask();
+        task();
+      }
+    }
+
+    public addTask(task:Task) {
+      this.taskQueue.push(task);
+    }
+
+    private hasNextTask():boolean {
+      return this.taskQueue.length > 0;
+    }
+
+    private nextTask():Task {
+      if (this.hasNextTask()) {
+        return this.taskQueue.shift();
+      } else {
+        throw new NoNextTaskError();
+      }
+    }
+  }
+  class TaskError extends Error {
+    public name = "TaskError";
+
+    constructor(public message?:string) {
+      super(message);
+    }
+  }
+  class NoNextTaskError extends TaskError {
+    public name = "NoNextTaskError";
+  }
+
+}
+
+
+function main_init() {
+  comm.log("stub_test:start");
+  var id = "98765432";
+  var password = "123456";
+  social_connection.asynchronous_logic.login(id, password);
+  comm.log("stub_test:end");
+}
