2390439cb1802a977d2652819aaf516a4304c9f6
Beeno Tung <aabbcc1241@yahoo.com.hk>
2015-12-17 01:39:05 +0800

    to show profile when click on reply

--- public/js/ListCtrl.js
+++ public/js/ListCtrl.js
@@ -66,11 +66,20 @@ app.controller("ListCtrl", function ($scope, $http, $global, $uibModal) {
         var elements = dataObject_array.map(function (dataObject) {
           return dataObject.toObject();
         });
+        var n = elements.length;
+        var displayName = getDisplayNameFromAction(action);
+        if (n == 0)
+          $scope.text_before_list = "There are no " + displayName + "currently. Come again later";
+        else if (n == 1)
+          $scope.text_before_list = "There is 1 " + displayName;
+        else
+          $scope.text_before_list = "There are " + n + " " + displayName;
         $scope.elems = elements;
       };
       stub_instance.use_all_instance_list(consumer);
     }
   };
+
   $scope.loadExtraElements = function (action) {
     if (action == null)
       throw new Error("ListCtrl::loadExtraElements param action must be string! (now is null)");
@@ -85,9 +94,25 @@ app.controller("ListCtrl", function ($scope, $http, $global, $uibModal) {
     }
   };
 
+  $scope.loadExtraSubElements = function (action, elem) {
+    if (action == null)
+      throw new Error("ListCtrl::loadExtraElements param action must be string! (now is null)");
+    if (elem == null)
+      throw new Error("ListCtrl::loadExtraElements param elem must not be null!");
+    utils.log("loading Extra elements of " + action);
+    var target = action.toLowerCase();
+    switch (target) {
+      case 'reply':
+        loadReplySubExtra(elem);
+        break;
+      default:
+        throw new Error("ListCtrl : param action must be string! (now is null)");
+    }
+  };
+
   function loadReplyExtra() {
     utils.log("loading user info");
-    $scope.myExtra.creator = "loading creator...";
+    $scope.myExtra.creator = "loading creator name...";
     /* get user instance */
     var target_user_id = $scope.$parent.selectedItem.get_creator_account_id();
     var user_stub = new stub.User_stub();
@@ -111,8 +136,45 @@ app.controller("ListCtrl", function ($scope, $http, $global, $uibModal) {
             $scope.myExtra.creator = "Cannot found creator info";
           }
           else {
-            //TODO
             $scope.myExtra.creator = usernames[0].getDisplayName();
+          }
+        };
+        DataObjectManager.request(username_stub, usernameFilter, usernameConsumer);
+      }
+    };
+    DataObjectManager.request(user_stub, userFilter, usersConsumer);
+  }
+
+  function loadReplySubExtra(elem) {
+    utils.log("loading user info");
+    elem.replierName = "loading replier name...";
+    /* get reply instance */
+    var index = $scope.elems.indexOf(elem);
+    var reply = $scope.dataObjects[index];
+    var target_user_id = reply.get_creator_account_id();
+    var user_stub = new stub.User_stub();
+    var userFilter = function (user) {
+      return user.get_account_id() == target_user_id;
+    };
+    var usersConsumer = function (users) {
+      if (users.length == 0) {
+        utils.log("no matched user info");
+        elem.replierName = "Cannot found replier info";
+      } else {
+        /* get username instance */
+        utils.log("loading username");
+        var username_stub = new stub.Username();
+        var usernameFilter = function (username) {
+          return username.get_account_id() == target_user_id;
+        };
+        var usernameConsumer = function (usernames) {
+          if (usernames.length == 0) {
+            utils.log("no matched username");
+            elem.replierName = "Cannot found replier info";
+          }
+          else {
+            //TODO
+            elem.replierName = usernames[0].getDisplayName();
           }
         };
         DataObjectManager.request(username_stub, usernameFilter, usernameConsumer);
@@ -122,6 +184,26 @@ app.controller("ListCtrl", function ($scope, $http, $global, $uibModal) {
     //TODO
   }
 
+  function getDisplayNameFromAction(action) {
+    //TODO replaced by hard-code part for best user-friendly
+    if (action == null || action == "" || action instanceof Array)
+      throw Error("getDisplayNameFromAction : invalid param action! Should be string");
+    switch (action) {
+      case "DiscussBoard":
+        return "Discuss Board";
+      default:
+        /* attempt to 'prettify' the display name */
+        var name = action[0];
+        var actionL = action.toUpperCase();
+        var n = action.length;
+        for (var i = 1; i < n; i++) {
+          if (action[i] == actionL[i])
+            name += " ";
+          name += action[i]
+        }
+        return name;
+    }
+  }
 
 });
 
