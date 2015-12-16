5ded0e72112fa4e240a46377fa1c43f40d9e14b4
Beeno Tung <aabbcc1241@yahoo.com.hk>
2015-12-16 17:09:28 +0800

    restored ts files

---
+++ public/ts/debug.ts
@@ -0,0 +1,39 @@
+///<reference path="utils.ts"/>
+module debug {
+  export class APIError extends Error {
+    public name = "APIError";
+
+    constructor(message?:string) {
+      super(message);
+    }
+  }
+
+  export class IllegalStatusError extends Error {
+    public name = "IllegalStatusError";
+
+    constructor(message?:string) {
+      super(message);
+    }
+  }
+  export class APIFailedError extends Error {
+    public name = "APICallFailed";
+
+    constructor(message?:string) {
+      super(message);
+    }
+  }
+  export class APIParseResultError extends APIError {
+    public name = "APIParseResultError";
+
+    constructor(message?:string) {
+      super(message);
+    }
+  }
+  export class APICallError extends APIError {
+    public name = "APICallError";
+
+    constructor(message?:string) {
+      super(message);
+    }
+  }
+}
