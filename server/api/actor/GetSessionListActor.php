cf02477a0845b940f66b2e4d5a2f3d3896fd2b97
Beeno Tung <aabbcc1241@yahoo.com.hk>
2015-12-18 19:25:07 +0800

    updated get [Exhibition|Attracion|Session] list actor using get event list actor

--- server/api/actor/GetSessionListActor.php
+++ server/api/actor/GetSessionListActor.php
@@ -7,33 +7,25 @@
 class GetSessionListActor extends Actor
 {
     public $name = "GetSessionList";
-    public $params = array(
-        /* message id */
-        APIFieldEnum::_id_array => []
-    );
+    public $params = [
+        APIFieldEnum::_id_array => [1, 2, 3, 4, 5]
+    ];
     public $output = [
         APIFieldEnum::_result_code => ResultCodeEnum::_Success,
-        /* array of message + profile */
-        APIFieldEnum::_element_array => []
+        APIFieldEnum::_element_array => [],
     ];
-    public $desc = "Get inbox message of a given user, the array of 'message element' are message+profile";
+    public $desc = "Get list of Session specified by client, used when scrolling or click 'show more' to avoid downloading too many (large) Session";
 
     public function handle($data)
     {
         $account_id = ActorUtil::check_session_valid($data);
         put_all_into($data, $this->params);
-        $message_id_array = $this->params[APIFieldEnum::_id_array];
 
-        $where_statement = "WHERE (" . Session_Fields::_ . "." . Session_Fields::__from_account_id . " = " . $account_id . ")";
-        $N_msg_id = count($message_id_array);
-        if ($N_msg_id > 0) {
-            $extra_where_statement = Session_Fields::_ . '.' . Session_Fields::__msg_id . "=" . $message_id_array[0];
-            for ($i = 1; $i < $N_msg_id; $i++) {
-                $extra_where_statement .= " OR " . Session_Fields::_ . '.' . Session_Fields::__msg_id . '=' . $message_id_array[$i];
-            }
-            $where_statement = "$where_statement AND ($extra_where_statement)";
-        }
-        $result = DatabaseHelper::select_from_table(Session_Fields::_, [], $where_statement);
+        $pass_data = $data;
+        $pass_data[APIFieldEnum::_id_array] = $this->params[APIFieldEnum::_id_array];
+        $pass_data[Event_Fields::__event_type] = event_type_Enum::__S;
+        $actor = new GetEventListActor();
+        $result = $actor->handle($pass_data);
 
         $this->output[APIFieldEnum::_element_array] = $result;
         return $this->output;
