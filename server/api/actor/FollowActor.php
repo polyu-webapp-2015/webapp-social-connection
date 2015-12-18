611db23975e2c0a3c633105c91168b1f4651935a
Beeno Tung <aabbcc1241@yahoo.com.hk>
2015-12-18 23:04:29 +0800

    updated followactor param key name

--- server/api/actor/FollowActor.php
+++ server/api/actor/FollowActor.php
@@ -8,7 +8,7 @@ class FollowActor extends Actor
 {
     public $name = "Follow";
     public $params = array(
-        Follow_Fields::__followed_account_id => "124"
+        Account_Fields::__account_id => "124"
     );
     public $output = [
         APIFieldEnum::_result_code => ResultCodeEnum::_Success,
@@ -20,7 +20,7 @@ class FollowActor extends Actor
     {
         $source_account_id = ActorUtil::check_session_valid($data);
         put_all_into($data, $this->params);
-        $dest_user_id = $this->params[Follow_Fields::__followed_account_id];
+        $dest_user_id = $this->params[Account_Fields::__account_id];
         if ($dest_user_id == $source_account_id)
             throw new Exception("User following it's self", ResultCodeEnum::_Logic_Error);
         $field_value_array = [
