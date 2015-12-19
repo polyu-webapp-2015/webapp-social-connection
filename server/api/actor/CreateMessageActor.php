14b8ec98a104774d376009fe2e813ca6868beeb0
Beeno Tung <aabbcc1241@yahoo.com.hk>
2015-12-19 15:40:00 +0800

    added message in response of create_message_actor

--- server/api/actor/CreateMessageActor.php
+++ server/api/actor/CreateMessageActor.php
@@ -15,14 +15,15 @@ class CreateMessageActor extends Actor
     );
     public $output = [
         APIFieldEnum::_result_code => ResultCodeEnum::_Success,
-        APIFieldEnum::_latest_id => 123
+        APIFieldEnum::_latest_id => 123,
+        APIFieldEnum::_message => []
     ];
     public $desc = "send message to another user";
 
     public function handle($data)
     {
         $sender_account_id = ActorUtil::check_session_valid($data);
-        put_all_into($data,$this->params);
+        put_all_into($data, $this->params);
         $receiver_account_id = $this->params[Message_Fields::__to_account_id];
         if ($sender_account_id == $receiver_account_id)
             throw new Exception("The message receiver is the sender", ResultCodeEnum::_Logic_Error);
@@ -32,7 +33,15 @@ class CreateMessageActor extends Actor
             Message_Fields::__msg_content => $this->params[Message_Fields::__msg_content]
         ];
         $result = DatabaseHelper::table_insert(Message_Fields::_, $field_value_array);
-        $this->output[APIFieldEnum::_latest_id] = DatabaseHelper::lastInsertId();
+        $lastInsertId = DatabaseHelper::lastInsertId();
+        $this->output[APIFieldEnum::_latest_id] = $lastInsertId;
+
+        /* get new inserted message */
+        $key = Message_Fields::__msg_id;
+        $rows = DatabaseHelper::select_from_table(Message_Fields::_, [], "WHERE $key = $lastInsertId");
+        $message = $rows[0];
+        $this->output[APIFieldEnum::_message] = $message;
+
         return $this->output;
     }
 }
